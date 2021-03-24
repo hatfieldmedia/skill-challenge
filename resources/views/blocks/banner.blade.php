7a32d3aef45f117ff1b75e388052fb058184b40c
morgan <morgan.brown@hatfieldmedia.com>
2021-03-24 16:53:26 -0400

    one-colum

--- resources/views/blocks/banner.blade.php
+++ resources/views/blocks/banner.blade.php
@@ -1,5 +1,4 @@
 <div class="block banner-block">
-    <h2 class="text-center">This file is the Banner block.</h2>
-    <p class="text-center mb-0">File Located: /resources/views/blocks/banner.blade.php</p>
-    <p class="text-center">Sass Located: /resources/blocks/_banner.scss</p>
+  <div class="background-image"></div>
+    <h2 class="text">THIS IS A <br/>WEBPAGE</h2>
 </div>
--- resources/views/skill-challenge.blade.php
+++ resources/views/skill-challenge.blade.php
@@ -1,7 +1,7 @@
 @extends('app')
 
 @section('content')
-    <h1 class="text-center mt-5">Welcome to the Skill Challenge</h1>
+    <!-- <h1 class="text-center mt-5">Welcome to the Skill Challenge</h1>
     <p class="text-center mb-5 w-50 mx-auto">This skill challenge is created as a way to assess skills, design, and interpretation of design into a web site.  Some portion of the desktop mockups will be turned into mobile mockups, but the sections that are not I want you to interpret to fit a mobile page.  This page should be fully responsive.  Do not try to copy the mockup to the pixel dimensions.  Make sure that the site <</p>
     <p class="text-center mb-0">Main File Located: /resources/views/skill-challenge.blade.php</p>
     <p class="text-center mb-5">It holds all other files/partials that you need to complete this.</p>
@@ -10,9 +10,9 @@
     <p class="text-center mb-0">JS File Located: /resources/js/custom.js</p>
     <p class="text-center mb-0">Main SASS File Located: /resources/sass/_theme.scss</p>
     <p class="text-center mb-0">Variables SASS File Located: /resources/sass/_variables.scss</p>
-    <p class="text-center mb-5">Typography SASS File Located: /resources/sass/_typography.scss</p>
+    <p class="text-center mb-5">Typography SASS File Located: /resources/sass/_typography.scss</p> -->
 
-    
+
     @include('blocks.banner')
     @include('blocks.one-column')
     @include('blocks.two-column-icon')
